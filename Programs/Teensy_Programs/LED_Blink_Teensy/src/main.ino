9d28ebb1e5d1ce77f3a32e94dee5459f90ec8916
vaishnav_sabari_girish <forgamesonly684@gmail.com>
2025-03-01 21:02:30 +0530

    feat(LED_Program): LED Blinking Program with Serial Output

---
+++ Programs/Teensy_Programs/LED_Blink_Teensy/src/main.ino
@@ -0,0 +1,13 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, 1);
+  Serial.println("LED ON");
+  delay(1000);
+  digitalWrite(13, 0);
+  Serial.println("LED OFF");
+  delay(2000);
+}
